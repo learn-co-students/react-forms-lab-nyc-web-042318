8b2a7d83e83cc89587be642f177fa949d5eadf2a
blevm <mjblevins7@gmail.com>
2018-06-26 14:35:47 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,48 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      userValue: '',
+      passwordValue: ''
+    };
+  }
+
+  updateUsername = (event) => {
+    this.setState({
+      userValue: event.target.value
+    })
+
+  }
+
+  updatePassword = (event) => {
+    this.setState({
+      passwordValue: event.target.value
+    })
+  }
+
+  whenSubmitting = (event) => {
+    event.preventDefault()
+    if (this.state.userValue !== '' && this.state.passwordValue !== '') {
+      return this.props.onSubmit()
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.whenSubmitting}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.userValue} onChange={this.updateUsername}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.passwordValue} onChange={this.updatePassword} />
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,34 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
+  }
+
+  updateTextarea = (event) => {
+    // console.log(event.target.value.split('\n').length)
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  displayError = () => {
+    if (this.state.value.split('\n').length !== 3 || this.state.value === '' ) {
+      return (
+        <div id="poem-validation-error" style={{ color: "red" }}>
+          This poem is not written in the right structure!
+        </div>
+      )
+    }
+
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60" value={this.state.value} onChange={this.updateTextarea}/>
+        {this.displayError()}
       </div>
     );
   }
